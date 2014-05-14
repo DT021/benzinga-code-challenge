fe92b3c5110fb48903e9b932a3cd25ff4a4bac52
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-05-14 03:22:54 -0400

    started functionality in python

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, session
+import urllib2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+    # return urllib2.urlopen('http://data.benzinga.com/stock/F')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ transactions.py
@@ -0,0 +1,52 @@
+import urllib2
+import json
+
+cookie = False
+
+if cookie:
+	balance = cookie.balance
+	portfolio = cookie.portfolio
+else:
+	balance = 100000
+	portfolio = []
+
+def get_info(stock):
+    """Gets all relevant info for a stock in puts it all into a dictionary
+    """
+    raw_data = urllib2.urlopen('http://data.benzinga.com/stock/' + stock)
+    all_info = json.load(raw_data)
+
+    if 'status' in all_info and 'msg' in all_info:
+    	return 'Symbol not found'
+    
+    stock_info = {}
+    stock_info['name'] = all_info['name']
+    stock_info['symbol'] = all_info['symbol']
+    stock_info['name'] = all_info['name']
+    stock_info['ask_price'] = all_info['ask']
+    stock_info['bid_price'] = all_info['bid']
+    stock_info['quantity'] = 0
+    return stock_info
+
+def buy_stock(stock_info, quantity):
+	if balance - stock_info.ask_price * quantity < 0:
+		return "Not enough money to perform transaction"
+	stock_info['quantity'] += quantity
+	balance -= stock_info['ask_price'] * quantity 
+	portfolio.append(stock_info)
+	
+
+def sell_stock(stock_info, quantity):
+	if quantity > stock_info['quantity']:
+		return "You do not have enough of this stock to sell"
+	stock_info['quantity'] -= quantity
+	balance += stock_info['bid_price'] * quantity 
+	portfolio.append(stock_info)
+
+def get_balance():
+	return balance
+
+def get_portfolio():
+	return portfolio
+
+print get_info('GOOG')
